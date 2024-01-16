4b0079f6eaf1663785228f4ac06f3df89aa866cf
nothhhing <lynchlm064@gmail.com>
2024-01-15 20:54:18 -0500

    better logs

--- index.ts
+++ index.ts
@@ -15,13 +15,13 @@ async function main() {
         const startTime = performance.now();
         const content = await Bun.file(new_dump).text();
         const elapsedTime = performance.now() - startTime;
-        console.log(
-          chalk.grey(`this.content(${new_dump}): ${elapsedTime.toFixed(3)}ms`),
-        );
-        return content;
+        return {
+          content,
+          str: `readDumpFile(${new_dump}): ${chalk.blue(elapsedTime.toFixed(3))}ms`,
+        };
       } catch (error: any) {
         console.error("Error reading file:", error);
-        return "wat";
+        return { content: "failed", str: "failed" };
       }
     })();
 
@@ -40,11 +40,19 @@ async function main() {
       /\/\/ RVA: 0x3[A-F0-9]+.*\n\s+public bool \S+\(Nullable<.*FreeSpin.*\n\n\s+\/\/ RVA: (0x3[A-F0-9]+)/g,
       /(0x2[A-F0-9]+).*\n\s+internal static bool \S+ \S+.*\n\n.*\n.*\n.*\n.*\n.*\n\n.*\n\s+internal static void.*bool.*= True.*\n\n.*\n.*\n\n.*\n\s+internal static Offer/g,
       /internal int \S+\(\).*\n\n.*\n\s+internal int \S+\(\).*\n\n.*\n\s+internal void \S+\(int \S+.*\n\n\s+\/\/ RVA: (0x2[A-F0-9]+).*\n\s+internal bool \S+.*\n\n\s+\/\/ RVA: (0x2[A-F0-9]+).*\n\s+internal bool \S+.*\n\n\s+\/\/ RVA: (0x2[A-F0-9]+).*\n\s+internal bool \S+/g,
-      /internal void \.ctor\(Dictionary<string\, object> \S+.*\n\n.*\n\s+internal Dictionary<string, object> \S+\(\).*\n\n\s+\/\/ RVA: (0x2[A-F0-9]).*\n\s+internal bool/g,
+      /internal void \.ctor\(Dictionary<string\, object> \S+.*\n\n.*\n\s+internal Dictionary<string, object> \S+\(\).*\n\n\s+\/\/ RVA: (0x2[A-F0-9]+).*\n\s+internal bool/g,
+      /\[CompilerGenerated\]\n\s+\/\/ RVA: 0x5.*\n\s+internal int \S+\(\).*\n\n\s+\[CompilerGenerated\]\n.*\n\s+private void \S+\(int \S+\).*\n\n\s+\/\/ RVA: (0x5[A-F0-9]+).*\n\s+internal bool/g,
+      /\[CompilerGenerated\]\n.*\n\s+internal static void \S+\(Action<string> \S+.*\n\n\s+\/\/ RVA: 0x2.*\n\s+internal static GameOb.*\n\n\s+\/\/ RVA: (0x[A-F0-9]+)/g,
+      /internal ModuleData\.ModuleType \S+\(\).*\n\n\s+\/\/ RVA: 0x2.*\n\s+internal ModuleData\.ModuleCategory \S+\(\).*\n\n.*\n\s+internal int.*\n\n.*\n\s+internal.*\n\n\s+\/\/ RVA: (0x[A-F0-9]+)/g,
+      /internal void \.ctor\(int \S+ ModuleData.ModuleRarity .*\n\n.*\n\s+internal bool.*\n\n\s+\/\/ RVA: (0x[A-F0-9]+)/g,
     ];
+    const totalStartTime = performance.now(); // Track total time
 
     const pushOffset = (pattern: RegExp) => {
-      const match = pattern.exec(newFile);
+      const patternStartTime = performance.now();
+      const match = pattern.exec(newFile.content);
+      const patternElapsedTime = performance.now() - patternStartTime;
+
       if (match && match[1] !== null) {
         newOffsets.push({
           offset: match[1],
@@ -58,7 +66,13 @@ async function main() {
 
     regexPatterns.forEach(pushOffset);
 
+    const totalElapsedTime = performance.now() - totalStartTime;
+    const averageTime = totalElapsedTime / regexPatterns.length;
     console.log(newOffsets);
+    console.log(chalk.grey(newFile.str));
+    console.log(
+      chalk.grey(`Average execution time: ${chalk.blue(averageTime.toFixed(3))}ms`),
+    );
   } catch (error) {
     console.error("An error occurred:", error);
   }
