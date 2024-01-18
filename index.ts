05ae01d6ba66eb8a20e10a1a0797e7325fe0adc5
nothhhing <lynchlm064@gmail.com>
2024-01-17 19:53:06 -0500

    more offsets

--- index.ts
+++ index.ts
@@ -45,18 +45,27 @@ async function main() {
       /internal static int \S+\(int.*\n\n\s+\/\/ RVA: 0x5[A-F0-9]+.*\n\s+internal static float \S+\(int.*\n\n.*\n\s+internal static int.*\n\n.*\n\s+internal static int.*\n\n.*\n\s+internal static float.*\n\n\s+\/\/ RVA: (0x[A-F0-9]+)/g, // fire rate
       /internal static bool \S+\(\).*\n\n.*\n\s+internal static float \S+\(\).*\n\n\s+\/\/ RVA: (0x[A-F0-9]+).*\n\s+internal static bool \S+\(\).*/g, // inf ammo
       /internal static SceneInfo \S+\(\S+ \S+\).*\n\n.*\n\s+internal static int.*bool.*\n\n.*\n.*\n\n.*\n.*\n\n\s+\/\/ RVA: (0x[A-F0-9]+)/g, // team kill
+      /\s+\/\/ RVA: (0x1[A-F0-9]+).*\n\s+internal static bool \S+\(\).*\n\n.*\n\s+internal static bool \S+\(\).*\n\n.*\n\s+internal static bool \S+\(\).*\n\n.*\n\s+internal static bool \S+\(\).*\n\n.*\n\s+internal static bool \S+\(\).*\n\n.*\n\s+internal static bool \S+\(\).*\n\n\s+\/\/ RVA: (0x[A-F0-9]+).*\n\s+internal static void/g, // team kill
+      /internal void \S+\(WeaponSounds\.\S+ \S+ = 0.*\n\n.*\n.*\n\n\s+\/\/ RVA: (0x4[A-F0-9]+)/g, // god
+      /private void \S+\(ref float.*\n\n.*\n.*\n\n\s+\/\/ RVA: (0x4[A-F0-9]+)/g, // god
+      /private void OnDestroy\(\).*\n\n\s+\/\/ RVA: (0x5[A-F0-9]+).*\n\s+private void OnTriggerEnter\(/g, // god
+      /\/\/ RVA: (0x[A-F0-9]+).*\n\s+private void OnController.*\n\n.*\n\s+private void OnDestroy\(\).*\n\n\s+\/\/ RVA: 0x5[A-F0-9]+.*\n\s+private void OnTriggerEnter\(/g, // god
+      /internal static void \S+\(int \S+\).*\n\n\s+\/\/ RVA: (0x3[A-F0-9]+).*\n\s+internal static int \S+\(\).*\n\n.*\n\s+internal static void \S+\(int \S+\).*\n\n.*\n\s+internal static int \S+\(\).*\n\n.*\n\s+internal static void \S+\(int \S+\).*\n\n.*\s+internal static int.*\n\n.*\n\s+public void \.ctor/g, // arena score
+      /\/\/ RVA: (0x[A-F0-9]+).*\n\s+public int GetFreeCheckpointsCount\(\)/g, // free checkpoint count
+      /\/\/ RVA: (0x[A-F0-9]+).*\s+internal virtual itemRarity get_Rarity.*\n\n\s+\/\/ RVA: 0x[A-F0-9]+/g, //, rarity
+      /\/\/ RVA: 0x[A-F0-9]+.*\s+internal virtual itemRarity get_Rarity.*\n\n\s+\/\/ RVA: (0x[A-F0-9]+)/g, // rarity
     ];
 
     const newOffsets: OffsetInfo[] = [];
 
     const pushOffset = async (pattern: RegExp, index: number) => {
       const match: OffsetMatch | null = pattern.exec(
-        await newFile.content,
+        await newFile.content
       ) as OffsetMatch | null;
 
       if (match && match[1] !== null) {
         const oldType: string | null = await oldFile.findMethodType(
-          offsetInfo.offsets[names.indexOf(names[index])],
+          offsetInfo.offsets[names.indexOf(names[index])]
         );
         const newType: string | null = await newFile.findMethodType(match[1]);
         newOffsets.push({
@@ -78,8 +87,12 @@ async function main() {
     console.log(newOffsets, { count: newOffsets.length });
     console.log(
       chalk.grey(
-        `Average execution time: ${chalk.blue(averageTime.toFixed(3))}ms\nTotal execution time: ${chalk.blue(totalElapsedTime.toFixed(3))}ms`,
-      ),
+        `Average execution time: ${chalk.blue(
+          averageTime.toFixed(3)
+        )}ms\nTotal execution time: ${chalk.blue(
+          totalElapsedTime.toFixed(3)
+        )}ms`
+      )
     );
   } catch (error) {
     console.error("An error occurred:", error);
